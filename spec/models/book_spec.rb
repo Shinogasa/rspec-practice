874fc71d3dac7f56361de3f3c17d06f80eb7a498
Shinonome <cuulbulu@gmail.com>
2022-06-07 10:24:28 +0900

    Add: Rspec起動まで

---
+++ spec/models/book_spec.rb
@@ -0,0 +1,6 @@
+require 'rails_helper' # 設定ファイルrails_helper.rbを読み込むコードが全テストにあります
+
+RSpec.describe Book, type: :model do # Bookモデルのテストコードをブロック内に書いていきます
+  # ここにBookモデルのテストコードを書いていきます
+  pending "add some examples to (or delete) #{__FILE__}"
+end
